f2dd9c2d003043c77d9903fed8eeed751edb35f6
Pedro Santana <pedroasantanav@hotmail.com>
2024-07-20 15:42:38 -0400

    App finished

--- worker.py
+++ worker.py
@@ -21,59 +21,68 @@ chat_history = []
 llm_hub = None
 embeddings = None
 
-Watsonx_API = "Your WatsonX API"
-Project_id= "Your Project ID"
+Watsonx_API = ""
+Project_id= "skills-network"
 
 # Function to initialize the language model and its embeddings
 def init_llm():
     global llm_hub, embeddings
     
     params = {
-        GenParams.MAX_NEW_TOKENS: 250, # The maximum number of tokens that the model can generate in a single run.
-        GenParams.MIN_NEW_TOKENS: 1,   # The minimum number of tokens that the model should generate in a single run.
-        GenParams.DECODING_METHOD: DecodingMethods.SAMPLE, # The method used by the model for decoding/generating new tokens. In this case, it uses the sampling method.
+        GenParams.MAX_NEW_TOKENS: 500, # The maximum number of tokens that the model can generate in a single run.
         GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
-        GenParams.TOP_K: 50,          # The top K parameter restricts the token generation to the K most likely tokens at each step, which can help to focus the generation and avoid irrelevant tokens.
-        GenParams.TOP_P: 1            # The top P parameter, also known as nucleus sampling, restricts the token generation to a subset of tokens that have a cumulative probability of at most P, helping to balance between diversity and quality of the generated text.
+        #GenParams.MIN_NEW_TOKENS: 1,   # The minimum number of tokens that the model should generate in a single run.
+        #GenParams.DECODING_METHOD: DecodingMethods.SAMPLE, # The method used by the model for decoding/generating new tokens. In this case, it uses the sampling method.
+        #GenParams.TOP_K: 50,          # The top K parameter restricts the token generation to the K most likely tokens at each step, which can help to focus the generation and avoid irrelevant tokens.
+        #GenParams.TOP_P: 1            # The top P parameter, also known as nucleus sampling, restricts the token generation to a subset of tokens that have a cumulative probability of at most P, helping to balance between diversity and quality of the generated text.
     }
     
-    credentials = {
+    my_credentials = {
         'url': "https://us-south.ml.cloud.ibm.com",
-        'apikey' : Watsonx_API
+        #'apikey' : Watsonx_API
     }
     
     LLAMA2_model = Model(
         model_id= 'meta-llama/llama-2-70b-chat',
-        credentials=credentials,
+        credentials=my_credentials,
         params=params,
         project_id=Project_id)
 
     llm_hub = WatsonxLLM(model=LLAMA2_model)
 
     #Initialize embeddings using a pre-trained model to represent the text data.
-    embeddings =  # create object of Hugging Face Instruct Embeddings with (model_name,  model_kwargs={"device": DEVICE} )
+    # create object of Hugging Face Instruct Embeddings with (model_name,  model_kwargs={"device": DEVICE} )
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": DEVICE}
+    )
 
 # Function to process a PDF document
 def process_document(document_path):
     global conversation_retrieval_chain
+
     # Load the document
-    loader =   # ---> use PyPDFLoader and document_path from the function input parameter <---
-    
+    loader = PyPDFLoader(document_path)
     documents = loader.load()
-    # Split the document into chunks, set chunk_size=1024, and chunk_overlap=64. assign it to variable text_splitter
-    text_splitter = # ---> use Recursive Character TextSplitter and specify the input parameters <---
-    
+
+    # Split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
     texts = text_splitter.split_documents(documents)
-    
+
     # Create an embeddings database using Chroma from the split text chunks.
     db = Chroma.from_documents(texts, embedding=embeddings)
-    
-    # Build the QA chain, which utilizes the LLM and retriever for answering questions.
+
+    # --> Build the QA chain, which utilizes the LLM and retriever for answering questions. 
+    # By default, the vectorstore retriever uses similarity search. 
+    # If the underlying vectorstore support maximum marginal relevance search, you can specify that as the search type (search_type="mmr").
+    # You can also specify search kwargs like k to use when doing retrieval. k represent how many search results send to llm
     conversation_retrieval_chain = RetrievalQA.from_chain_type(
         llm=llm_hub,
         chain_type="stuff",
-        retriever= db.as_retriever(search_type="mmr", search_kwargs={'k': 6, 'lambda_mult': 0.25}),
-        return_source_documents=False
+        retriever=db.as_retriever(search_type="mmr", search_kwargs={'k': 6, 'lambda_mult': 0.25}),
+        return_source_documents=False,
+        input_key = "question"
+     #   chain_type_kwargs={"prompt": prompt} # if you are using prompt template, you need to uncomment this part
     )
 
 
@@ -81,17 +90,17 @@ def process_document(document_path):
 def process_prompt(prompt):
     global conversation_retrieval_chain
     global chat_history
+    # Query the model
     # Pass the prompt and the chat history to the conversation_retrieval_chain object
     output = conversation_retrieval_chain({"question": prompt, "chat_history": chat_history})
-    
     answer =  output["result"]
     
     # Update the chat history
-    # TODO: Append the prompt and the bot's response to the chat history using chat_history.append and pass `prompt` `answer` as arguments
-    # --> write your code here <--	
+    # Append the prompt and the bot's response to the chat history
+    chat_history.append((prompt, answer))
     
     # Return the model's response
-    return result['answer']
+    return answer
     
 
 # Initialize the language model
